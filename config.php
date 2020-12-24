190d385b81cc93bec7fd8638589750cb146224ce
osiams <t20150715@gmail.com>
2020-12-24 11:48:40 +0700

    cd2

--- config.php
+++ config.php
@@ -1,28 +1,48 @@
 <?php
+#คำเตือน!! โปรดแก้ไขด้วยความระมัดระวัง
 define("CF", [
-	"a"=>["product","unit","fetch","me","user","setting","bills","sell","barcode","bill58","ret","it","day","qrc"],
-//==========คำเตือน!! โปรดแก้ไขด้วยความระมัดระวัง
-//==========ป้อมูลการเชื่อมต่อกับ ฐานข้อมูล MariaDB
-	"server"=>"127.0.0.1",					//--ค่านี้เท่านั้น
-	"database"=>"diypos_0.0",		//--ชื่อฐานข้อมูลตามรุ่น หรือ ชื่ออื่น
-	"user"=>"user",									//--ชื่อผู้ใช
-	"password"=>"password",			//--รหัสผ่านที่ได้ตั้งไว้ใน User accounts ของ   phpmyadmin
-//==http port ต้องตรงกับในไฟล์ httpd.conf
-		"http_port"=>80,
-		"https_port"=>443,
-//==========ตำแหน่งผู้ใช้ ลำดับตามบรรทัดมี 10 ระดับเท่านั้น
-"userceo"=>[
-	["name"=>"พักหรือออก","a"=>[]],
-	["name"=>"พนักงานขาย","a"=>["sell","me","fetch","product","bills","bill58","barcode","qrc"]],
-	null,
-	null,
-	null,
-	null,
-	null,
-	null,
-	["name"=>"ผู้จัดการรร้าน","a"=>["product","unit","fetch","me","user","bills","sell","bill58","ret","it","day","barcode","qrc"]],
-	["name"=>"เจ้าของร้าน","a"=>["product","unit","fetch","me","user","setting","bills","sell","bill58","ret","it","day","barcode","qrc"]]
-]
+#หน้า index?a=value ทั้งหมดที่มีและใช้งาน
+	"a"=>["product","unit","fetch","me","user","setting","bills","sell","barcode","bill58","ret","it","day","qrc","cd"],
+#้ข้อมูลการเชื่อมต่อกับ ฐานข้อมูล MariaDB
+	"server"=>"127.0.0.1",
+	"database"=>"diypos_0.0",
+	"user"=>"diypos",		
+	"password"=>"mr12345678",	
+#http port ต้องตรงกับในไฟล์ httpd.conf
+	"http_port"=>80,
+	"https_port"=>443,
+#เวลาที่เครื่องเซิฟเวอร์	
+	"timezone"=>"Asia/Bangkok",
+#ตำแหน่งผู้ใช้ ลำดับตามบรรทัดมี 10 ระดับเท่านั้น
+	"userceo"=>[
+		["name"=>"[[SYSTEM]]","a"=>["cd","me"]],
+		["name"=>"พักหรือออก","a"=>[]],
+		["name"=>"พนักงานขาย","a"=>["sell","me","fetch","product","bills","bill58","barcode","qrc"]],
+		["name"=>"-","a"=>["me"]],
+		["name"=>"-","a"=>["me"]],
+		["name"=>"-","a"=>["me"]],
+		["name"=>"-","a"=>["me"]],
+		["name"=>"-","a"=>["me"]],
+		["name"=>"ผู้จัดการรร้าน","a"=>["product","unit","fetch","me","user","bills","sell","bill58","ret","it","day","barcode","qrc"]],
+		["name"=>"เจ้าของร้าน","a"=>["product","unit","fetch","me","user","setting","bills","sell","bill58","ret","it","day","barcode","qrc"]]
+	]
+]);
+#ค่าการอณุญาตการใช้งาน
+#"user_regto" คือ สามารถลงทะเบียนผู้ใช้ใหม่ได้หรือไม่ [true=ได้,false=ไม่ได้]
+#"user_regceoto" คือ ระดับผู้ใช้ที่สามารถลงทะเบียนให้ได้ 
+#"user_editto" คือ ระดับผู้ใช้ที่สามารถแก้ไขได้
+#"user_delto" คือ ระดับผู้ใช้ที่ลบออกจากฐานข้อมูลได้
+define("PEM",[
+	0=>["user_regto"=>false,"user_regceoto"=>[],"user_editto"=>[],"user_delto"=>[]],
+	1=>["user_regto"=>false,"user_regceoto"=>[],"user_editto"=>[],"user_delto"=>[]],
+	2=>["user_regto"=>false,"user_regceoto"=>[],"user_editto"=>[],"user_delto"=>[]],
+	3=>["user_regto"=>false,"user_regceoto"=>[],"user_editto"=>[],"user_delto"=>[]],
+	4=>["user_regto"=>false,"user_regceoto"=>[],"user_editto"=>[],"user_delto"=>[]],
+	5=>["user_regto"=>false,"user_regceoto"=>[],"user_editto"=>[],"user_delto"=>[]],
+	6=>["user_regto"=>false,"user_regceoto"=>[],"user_editto"=>[],"user_delto"=>[]],
+	7=>["user_regto"=>false,"user_regceoto"=>[],"user_editto"=>[],"user_delto"=>[]],
+	8=>["user_regto"=>true,"user_regceoto"=>[1,2,3,4,5,6,7],"user_editto"=>[1,2,3,4,5,6,7],"user_delto"=>[1,2,3,4,5,6,7]],
+	9=>["user_regto"=>true,"user_regceoto"=>[1,2,3,4,5,6,7,8,9],"user_editto"=>[0,1,2,3,4,5,6,7,8,9],"user_delto"=>[0,1,2,3,4,5,6,7,8,9]]
 ]);
 
 ?>
